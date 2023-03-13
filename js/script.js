93c86a4dfba723fc6ffd5db06734c604eed07e05
marikadiblasio <marbla.albram@gmail.com>
2023-03-13 14:50:47 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+1) Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+2) Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+/* Procedimento:
+1)crea array di mail (da dove?!?) JS- form HTML -->  check lista valori input JS--> stampa messaggio e ricarica pagina (gioco o form) JS
+2)bottone da cliccare (?!)  --> genera n. utente --> bottone da cliccare --> genera n. pc --> confronta n e n --> genera e stampa messaggio
